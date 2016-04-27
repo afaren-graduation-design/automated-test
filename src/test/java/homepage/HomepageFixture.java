d2d4bb654f091d5bab597c7db3edee7a47015483
linwenjun <linwenjun@163.com>
2016-04-27 09:37:48 +0800

    pass the automatic test

---
+++ src/test/java/homepage/HomepageFixture.java
@@ -0,0 +1,42 @@
+package homepage;
+
+import org.concordion.integration.junit4.ConcordionRunner;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+@RunWith(ConcordionRunner.class)
+public class HomepageFixture {
+
+    public Boolean login(String account, String password) {
+        ChromeDriver driver = new ChromeDriver();
+
+        driver.get("http://192.168.99.100:8888/register.html#login");
+
+        (new WebDriverWait(driver, 10))
+                .until(ExpectedConditions.presenceOfElementLocated(By.name
+                        ("email")));
+
+        WebElement emailInput = driver.findElement(By.name("email"));
+        WebElement passwordInput = driver.findElement(By.name("loginPassword"));
+        WebElement captcha = driver.findElement(By.name("captcha"));
+        WebElement loginBtn = driver.findElement(By.id("login-btn"));
+
+        emailInput.sendKeys(account);
+        passwordInput.sendKeys(password);
+        captcha.sendKeys("1234");
+
+        loginBtn.click();
+
+        WebElement dropdownMenu = (new WebDriverWait(driver, 10))
+                .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@id='dropdownMenu1']/span[1]")));
+
+        return dropdownMenu.getText().equals(account);
+
+
+//        return password.equals("12345678");
+    }
+}
