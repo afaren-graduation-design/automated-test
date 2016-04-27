50efe9368b771eaeabce9bafbb380919009bf0e7
wengjiaojiao <419909058@qq.com>
2016-04-27 17:38:50 +0800

    selenium login and register

--- src/test/java/homepage/RegisterFixture.java
+++ src/test/java/homepage/RegisterFixture.java
@@ -9,34 +9,33 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 @RunWith(ConcordionRunner.class)
-public class HomepageFixture {
-
-    public Boolean login(String account, String password) {
+public class RegisterFixture {
+    public Boolean register(String mobilePhone, String email, String password) {
         ChromeDriver driver = new ChromeDriver();
 
-        driver.get("http://192.168.99.100:8888/register.html#login");
+        driver.get("http://192.168.99.100:8888/register.html#register");
 
         (new WebDriverWait(driver, 10))
                 .until(ExpectedConditions.presenceOfElementLocated(By.name
                         ("email")));
 
+        WebElement mobilePhoneInput = driver.findElement(By.name("mobilePhone"));
         WebElement emailInput = driver.findElement(By.name("email"));
-        WebElement passwordInput = driver.findElement(By.name("loginPassword"));
+        WebElement passwordInput = driver.findElement(By.name("password"));
         WebElement captcha = driver.findElement(By.name("captcha"));
-        WebElement loginBtn = driver.findElement(By.id("login-btn"));
+        WebElement degreeCheckbox = driver.findElement(By.className("agree-check"));
+        WebElement registerBtn = driver.findElement(By.id("register-btn"));
 
-        emailInput.sendKeys(account);
+        mobilePhoneInput.sendKeys(mobilePhone);
+        emailInput.sendKeys(email);
         passwordInput.sendKeys(password);
         captcha.sendKeys("1234");
-
-        loginBtn.click();
+        degreeCheckbox.click();
+        registerBtn.click();
 
         WebElement dropdownMenu = (new WebDriverWait(driver, 10))
                 .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@id='dropdownMenu1']/span[1]")));
 
-        return dropdownMenu.getText().equals(account);
-
-
-//        return password.equals("12345678");
+        return dropdownMenu.getText().equals(email);
     }
 }
--- src/test/resources/homepage/Login.md
+++ src/test/resources/homepage/Login.md
@@ -5,7 +5,3 @@ As a normal user, I want login the twars
 ### [Example](- "example1")
 
 I can login with the account [test@163.com] (- "#account") and password [12345678] (- "#password") [successfully](- "c:assert-true=login(#account, #password)")
-
-### [Example](- "example2")
-
-I can login with the account [test@163.com] (- "#account") and password [1234567890] (- "#password") [failed](- "c:assert-false=login(#account, #password)")
